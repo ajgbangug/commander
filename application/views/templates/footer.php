62a0426f7d436c56a875589a6e6d19138d1dfaec
AJ Bangug <ajgbangug@gmail.com>
2013-04-18 23:29:48 +0800

    removed the navbar at the footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,17 +1,3 @@
-    <div class="row">
-	    <div class="navbar navbar-inverse navbar-fixed-bottom">
-	    	<div class="navbar-inner">
-	    		<div class="container">
-	    			<ul class="nav pull-right">
-	    				<li><a href="https://github.com/ajgbangug/commander">GitHub repo</a></li>
-	    				<li><a href="http://docs.fabfile.org/en/1.6/">fabric</a></li>
-	    				<li><a href="http://ellislab.com/codeigniter">CodeIgniter</a></li>
-	    				<li><a href="http://docs.mongodb.org/manual/">mongoDB</a></li>
-	    			</ul>
-	    		</div>
-	    	</div>
-	    </div>
-	</div>
     <script src="<?php echo base_url("assets/bootstrap/js/bootstrap.min.js"); ?>"></script>
     </body>
 </html>
